1f9814b3a74659b643b5f6727c5c2d2d2acf9f1b
abhilasha009 <poojakusingh40@gmail.com>
2020-06-21 10:07:06 +0530

    pofo template- template pages

--- TemplateInfo_Personal.php
+++ TemplateInfo_Personal.php
@@ -22,12 +22,6 @@
                         <div class="col-12 d-flex flex-column justify-content-center text-center extra-small-screen page-title-large">
                             <!-- start page title -->
                             <h1 class="text-white-2 alt-font font-weight-600 letter-spacing-minus-1 margin-10px-bottom" style="text-align: left; font-size: 100px;">Personal</h1>
-                            <!-- <br><br><br>
-                            <div class="slider-text-middle padding-ten-left sm-padding-five-left" style="text-align: left;">
-                                <a href="#" class="btn btn-medium btn-transparent-white text-medium border-radius-4 lg-margin-15px-bottom d-table d-lg-inline-block md-margin-lr-auto">View Template <i class="fas fa-long-arrow-alt-right margin-5px-left text-deep-pink text-medium position-relative top-2" aria-hidden="true"></i></a>
-                                &nbsp;&nbsp;&nbsp;
-                                <a href="#" class="btn btn-medium btn-transparent-white text-medium border-radius-4 lg-margin-15px-bottom d-table d-lg-inline-block md-margin-lr-auto">Use Template <i class="fas fa-long-arrow-alt-right margin-5px-left text-deep-pink text-medium position-relative top-2" aria-hidden="true"></i></a>
-                            </div> -->
                             <!-- end page title --> 
                         </div>
                     </div>
@@ -52,35 +46,27 @@
                             <p>- Make a list with items you need to do before your trip starts: packing lists, passport updates, booking confirmations, etc.</p>
                             <p>- When people inevitably give you recommendations, keep them all in “Things To Do/Places to Eat” lists so you don’t forget.</p>
                             
-                            <a href="#" style="float: right;" class="btn btn-small btn-dark-gray text-small border-radius-4 lg-margin-15px-bottom d-table d-lg-inline-block md-margin-lr-auto">View Template <i class="fas fa-long-arrow-alt-right margin-5px-left text-deep-pink text-medium position-relative top-2" aria-hidden="true"></i></a>
+                    <?php
+
+                        if(isset($_SESSION['UserID']))
+                        {
+                     ?>
+                            <a href="Personal_template.php?Uid=<?php echo $_SESSION['UserID']; ?>" style="float: right;" class="btn btn-small btn-dark-gray text-small border-radius-4 lg-margin-15px-bottom d-table d-lg-inline-block md-margin-lr-auto">View Template <i class="fas fa-long-arrow-alt-right margin-5px-left text-deep-pink text-medium position-relative top-2" aria-hidden="true"></i></a>
+                    <?php
+                        }
+                        else
+                        {
+                    ?>
+                         <a href="personal_view.php" style="float: right;" class="btn btn-small btn-dark-gray text-small border-radius-4 lg-margin-15px-bottom d-table d-lg-inline-block md-margin-lr-auto">View Template <i class="fas fa-long-arrow-alt-right margin-5px-left text-deep-pink text-medium position-relative top-2" aria-hidden="true"></i></a> 
+                    <?php
+                        }
+                    ?>  
                             
                         </div>
                     </div>
                 </div>
             </section>
             <!-- end about section -->
-
-            <!-- start section -->
-           <!--  <section class="border-top border-color-extra-light-gray wow fadeIn">
-                <div class="container">
-                    <div class="row align-items-center">
-                        <div class="col-12 col-lg-4 text-center text-lg-left md-margin-40px-bottom sm-margin-30px-bottom wow fadeIn">
-                            <h3 class="text-light-gray alt-font font-weight-600 text-middle-line margin-five-bottom md-margin-5px-bottom w-50 md-width-100 letter-spacing-minus-3 md-text-middle-line">02</h3>
-                            <h5 class="text-uppercase alt-font text-extra-dark-gray font-weight-700 width-75 d-block mb-0 lg-width-90 md-width-100">Class Management</h5>
-                        </div>
-                        <div class="col-12 col-lg-4 col-md-6 sm-margin-30px-bottom wow fadeIn">
-                            <img class="padding-ten-right md-no-padding-right w-100" src="assets1/images/homepage-4-section2-img.jpg" alt="">
-                        </div>
-                        <div class="col-12 col-lg-4 col-md-6 text-center text-md-left md-padding-eight-left sm-padding-15px-left wow fadeIn">
-                            <p class="text-large text-extra-dark-gray">We have our own developers and technical producers who can confidently integrate & execute ideas.</p>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s. Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry's standard dummy text ever since the 1500s.</p>
-                            <p>Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been 1500s. Lorem Ipsum is simply dummy text.</p>
-                            <a href="team-classic.html" class="alt-font text-uppercase font-weight-600 text-link-extra-dark-gray text-deep-pink-hover text-small">Our Creative People <i class="fas fa-long-arrow-alt-right margin-5px-left text-deep-pink text-medium position-relative top-2" aria-hidden="true"></i></a>
-                        </div>
-                    </div>
-                </div>
-            </section> -->
-            <!-- end section -->
 
             <p class="wow fadeIn">
                 
---
+++ education_view.php
@@ -0,0 +1,53 @@
+<!doctype html>
+<html class="no-js" lang="en">
+    
+<!-- Mirrored from www.themezaa.com/html/pofo/single-project-page-02.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 30 May 2020 06:52:54 GMT -->
+<head>
+        <!-- title -->
+        <title>EaseWork - view Education Template</title>
+        <?php include_once("loadhead.php");?>
+        
+</head>
+
+<body>
+        <!-- start header -->
+        <?php include_once("header.php");?>
+        <!-- end header -->
+        
+        <!-- start education screenshot section -->
+        <section id="home-bottom" class="parallax one-fourth-screen wow fadeIn" data-stellar-background-ratio="0.2" style="background-image:url('assets1/images/Education.png');">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12">
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- end education screenshot section -->
+
+        <!-- start quote section -->
+        <section class="wow fadeIn">
+            <div class="container">
+                <div class="row">
+                    <div class="col-12 col-xl-7 mx-auto text-center">
+                        <i class="fas fa-quote-left icon-small text-deep-pink d-block margin-25px-bottom"></i>
+                        <h6 class="alt-font text-extra-dark-gray">To use this Template please Login first. </h6>
+                        <span class="text-uppercase text-extra-small alt-font letter-spacing-3 text-medium-gray">
+                            <a href="login.php">Login Now</a>
+                        </span>
+                    </div>
+                </div>
+            </div>
+        </section>
+        <!-- end quote section -->
+        
+        <!-- start footer --> 
+        <?php include_once("footer.php");?>
+        <!-- end footer -->
+        <!-- javascript libraries -->
+        <?php include_once("loadJs.php");?>
+        
+    </body>
+
+<!-- Mirrored from www.themezaa.com/html/pofo/single-project-page-02.html by HTTrack Website Copier/3.x [XR&CO'2014], Sat, 30 May 2020 06:54:12 GMT -->
+</html>
